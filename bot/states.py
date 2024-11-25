2161870f9c3b4e19f4489aff00a4efa4f5ed9c50
Artur Boyun <arturboyun@gmail.com>
2024-11-25 20:04:18 +0200

    Remove unused state classes from states.py

--- bot/states.py
+++ bot/states.py
@@ -3,17 +3,3 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Menu(StatesGroup):
     main = State()
-
-
-class Channels(StatesGroup):
-    menu = State()
-
-
-class AddChannel(StatesGroup):
-    channel_id = State()
-    check = State()
-    finish = State()
-
-
-class DeleteChannel(StatesGroup):
-    confirm = State()
